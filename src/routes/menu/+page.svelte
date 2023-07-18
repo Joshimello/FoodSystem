ee580d48996fdc9ebb9869311d3a49781bc65c03
Joshimello <joshualeanjw@gmail.com>
2023-07-18 11:35:06 +0800

    img + search

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  import { Avatar, Layout } from 'stwui'
-  
   export let data
   const { popularFood, shops } = data
+  let value = 'C'
+
+  $: searchResults = popularFood.filter(({ name }) => name.includes(value))
+
 </script>
 
 <div class="flex justify-between p-4">
@@ -13,13 +15,16 @@
     <div class="i-material-symbols-location-on-outline text-black text-2xl"/>
     <span class="text-xl">NTHU</span>
   </div>
-  <Avatar src="image.png">
-    <Avatar.Indicator slot="indicator"/>
-  </Avatar>
+  <button class="relative">
+    <div class="i-material-symbols-shopping-cart text-black text-3xl"/>
+    <div class="absolute -top-2 -right-2 bg-rose-400 rounded-full w-5 h-5 flex justify-center items-center text-white text-xs">
+      3
+    </div>
+  </button>
 </div>
 
 <div class="w-full px-4 pb-4">
-  <div class="rounded-3xl bg-rose-400 h-40 p-8">
+  <div class="rounded-3xl bg-rose-400 h-40 p-8 flex bg-contain bg-right bg-no-repeat">
     <div class="h-full flex flex-col justify-between w-max">
       <span class="text-3xl font-serif text-white block">
         Get 15% Off
@@ -34,9 +39,45 @@
 <div class="w-full px-4 mb-4">
   <div class="flex items-center bg-white rounded-3xl px-8 py-4 gap-4">
     <div class="i-material-symbols-search text-black text-2xl p-2"/>
-    <input class="w-full bg-transparent outline-none" placeholder="Search"/>
+    <input class="w-full bg-transparent outline-none" placeholder="Search" bind:value/>
   </div>
 </div>
+
+{#if value}
+<div class="w-full px-4 mb-4 flex flex-col gap-2">
+  <span class="text-2xl text-black font-bold p-2">
+    Search Results
+  </span>
+  {#each searchResults as { name, price, rating, img }}
+    <div class="w-full rounded-2xl bg-white flex p-4 gap-4">
+      <div class="h-20 w-60 rounded-2xl bg-gray-200 overflow-hidden">
+        <img class="object-cover w-full h-full" src="{ img }">
+      </div>
+      <div class="flex flex-col w-full justify-between py-2">
+        <span class="text-2xl font-serif">
+          { name }
+        </span>
+        <div class="flex justify-between">
+          <span>
+            <span class="text-2xl font-bold text-rose-400">
+              { price }
+            </span>
+            <span class="text-xl text-gray-400">
+              /meal
+            </span>
+          </span>
+          <span class="flex items-center gap-1">
+            <div class="i-material-symbols-star text-rose-400 text-xl"/>
+            <span class="text-xl text-gray-400">
+              { rating }
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  {/each}
+</div>
+{/if}
 
 <div class="w-full px-4 mb-4">
   <span class="text-2xl text-black font-bold p-2">
@@ -45,7 +86,9 @@
   <div class="flex pt-4 gap-4">
     {#each popularFood.slice(0, 3) as { name, price, rating, img }}
       <div class="w-full rounded-2xl bg-white flex-col p-4">
-        <div class="h-30 w-full rounded-2xl bg-gray-200 mb-4"/>
+        <div class="h-30 w-full rounded-2xl bg-gray-200 mb-4 overflow-hidden">
+          <img class="object-cover w-full h-full" src="{ img }">
+        </div>
         <div class="flex">
           <span class="text-2xl font-serif">
             { name }
@@ -73,7 +116,9 @@
   <div class="grid grid-cols-2 gap-4 mt-4">
     {#each popularFood.slice(3, 7) as { name, price, rating, img }}
       <div class="w-full rounded-2xl bg-white flex p-4 gap-4">
-        <div class="h-20 w-40 rounded-2xl bg-gray-200"/>
+        <div class="h-30 w-60 rounded-2xl bg-gray-200 overflow-hidden">
+          <img class="object-cover w-full h-full" src="{ img }">
+        </div>
         <div class="flex flex-col w-full justify-between py-2">
           <span class="text-2xl font-serif">
             { name }
@@ -105,9 +150,11 @@
     Shops
   </span>
   <div class="flex overflow-auto py-4 gap-4">
-    {#each shops as { name, priceRange, rating }}
+    {#each shops as { name, priceRange, rating, img }}
       <div class="w-60 rounded-2xl bg-white flex-shrink-0 p-4 flex flex-col">
-        <div class="h-30 w-full rounded-2xl bg-gray-200 mb-4"/>
+        <div class="h-30 w-full rounded-2xl bg-gray-200 mb-4 overflow-hidden">
+          <img class="object-cover w-full h-full" src="{ img }">
+        </div>
         <span class="text-2xl font-serif">
           { name }
         </span>
--- src/routes/menu/+page.ts
+++ src/routes/menu/+page.ts
@@ -5,43 +5,43 @@ const popularFood = [
     name: "Classic Cheeseburger",
     price: "$8.99",
     rating: "4.5/5",
-    img: "https://example.com/images/classic_cheeseburger.jpg"
+    img: "https://source.unsplash.com/-Ml9CpVkxUQ"
   },
   {
     name: "Spicy Chicken Tacos",
     price: "$10.49",
     rating: "4.7/5",
-    img: "https://example.com/images/spicy_chicken_tacos.jpg"
+    img: "https://source.unsplash.com/6WHl6T-fxU0"
   },
   {
     name: "Margherita Pizza",
     price: "$12.99",
     rating: "4.8/5",
-    img: "https://example.com/images/margherita_pizza.jpg"
+    img: "https://source.unsplash.com/AcA8moIiD3g"
   },
   {
     name: "Garlic Butter Shrimp Pasta",
     price: "$14.95",
     rating: "4.6/5",
-    img: "https://example.com/images/garlic_butter_shrimp_pasta.jpg"
+    img: "https://source.unsplash.com/eSeo6IzOV00"
   },
   {
     name: "Veggie Sushi Roll",
     price: "$9.50",
     rating: "4.4/5",
-    img: "https://example.com/images/veggie_sushi_roll.jpg"
+    img: "https://source.unsplash.com/-1GEAA8q3wk"
   },
   {
     name: "BBQ Pulled Pork Sandwich",
     price: "$11.99",
     rating: "4.7/5",
-    img: "https://example.com/images/bbq_pulled_pork_sandwich.jpg"
+    img: "https://source.unsplash.com/FZ4y_IDUJ6w"
   },
   {
     name: "Creamy Mushroom Risotto",
     price: "$13.75",
     rating: "4.9/5",
-    img: "https://example.com/images/creamy_mushroom_risotto.jpg"
+    img: "https://source.unsplash.com/fUAuV0btigU"
   }
 ]
 
@@ -49,37 +49,44 @@ const shops = [
   {
     name: "The Burger Joint",
     priceRange: "$",
-    rating: "4.5/5"
+    rating: "4.5/5",
+    img: "https://source.unsplash.com/random/?Restaurant&1"
   },
   {
     name: "Taco Time",
     priceRange: "$",
-    rating: "4.7/5"
+    rating: "4.7/5",
+    img: "https://source.unsplash.com/random/?Restaurant&2"
   },
   {
     name: "Pizza Palace",
     priceRange: "$$",
-    rating: "4.8/5"
+    rating: "4.8/5",
+    img: "https://source.unsplash.com/random/?Restaurant&3"
   },
   {
     name: "Pasta Place",
     priceRange: "$$",
-    rating: "4.6/5"
+    rating: "4.6/5",
+    img: "https://source.unsplash.com/random/?Restaurant&4"
   },
   {
     name: "Sushi Spot",
     priceRange: "$$",
-    rating: "4.4/5"
+    rating: "4.4/5",
+    img: "https://source.unsplash.com/random/?Restaurant&5"
   },
   {
     name: "BBQ Bistro",
     priceRange: "$$",
-    rating: "4.7/5"
+    rating: "4.7/5",
+    img: "https://source.unsplash.com/random/?Restaurant&6"
   },
   {
     name: "Risotto Restaurant",
     priceRange: "$$",
-    rating: "4.9/5"
+    rating: "4.9/5",
+    img: "https://source.unsplash.com/random/?Restaurant&7"
   }
 ]
 
